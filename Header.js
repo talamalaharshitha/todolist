31a38361bef79c8b6ffa0aada3f13bf5750fd6ff
talamalaharshitha <talamalaharshitha@gmail.com>
2024-07-04 20:27:32 +0530

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const HeaderFile = () => {
+    return (
+        <div className='header'>
+            <h1>Todo-List</h1>
+
+        </div>
+    )
+}
+export default HeaderFile;
